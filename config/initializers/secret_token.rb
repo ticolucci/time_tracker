718921bcb66cd590c10d36a234db87948f7b364a
Thiago Colucci <ticolucci@gmail.com>
2011-01-26 09:29:20 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTracker::Application.config.secret_token = 'bcb4876f89303c4acddfa1555e47158375e049336dc7301cbcf207c2bb158a85aa8c812742e96ba1a39a5243b784f08f59fe7e9f96ad6fd66368bf8a04744a67'
