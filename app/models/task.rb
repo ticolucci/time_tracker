c93c45d380b70744c316b55666575f10c03ef5c7
Thiago Colucci <ticolucci@gmail.com>
2011-01-26 11:45:35 -0200

    task belongs_to user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,5 @@
 class Task < ActiveRecord::Base
-    attr_accessible :description, :begin, :duration
+    attr_accessible :description, :begin, :duration, :user_id
     
     belongs_to :user
-    
-    before_create :add_user_id
-    
-    def add_user_id
-      self.user = current_user
-    end
 end
